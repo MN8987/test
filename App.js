43383b10f729985c9f646a840dde8ab13e70e612
MN8987 <167154118+MN8987@users.noreply.github.com>
2024-05-28 13:49:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import C1 from "./c1";
+import C2 from "./c2";
+
+export default function App()
+{
+  return(
+    <div>
+      <C2/>
+      <h1>React Example</h1>
+      <C1/>
+    </div>
+  );
+}
